f6553c72756fe1dc4e9b2331334dc4728a1f734e
Md Shaon Hossain Sani <md.shaon.sani@fau.de>
2023-12-29 17:28:07 +0100

    Add Swagger API Configuration

--- job_board_backend/urls.py
+++ job_board_backend/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('users/', include('users.urls')),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    
+    # Optional UI:
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
 ]
