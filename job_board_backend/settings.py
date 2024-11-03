3abdeca56cc9e7f3344287d078a05aec89b8cf67
CoKn <43541871+CoKn@users.noreply.github.com>
2024-11-03 14:14:47 +0100

    adjusting settings.py

--- job_board_backend/settings.py
+++ job_board_backend/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
     'api',
