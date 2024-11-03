6431877cabc45670807092c62e9d41aeb950d712
CoKn <43541871+CoKn@users.noreply.github.com>
2024-11-03 11:40:03 +0100

    adjusting settings.py

--- job_board_backend/settings.py
+++ job_board_backend/settings.py
@@ -68,8 +68,6 @@ MIDDLEWARE = [
     
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True   
-
 ROOT_URLCONF = 'job_board_backend.urls'
 
 TEMPLATES = [
@@ -137,7 +135,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
